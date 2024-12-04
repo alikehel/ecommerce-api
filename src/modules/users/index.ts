8184e43519d6bc5fb67a093f3269aa49a5c1b0dd
Ali Kehel <ali.saber.27.2@gmail.com>
2024-12-04 08:53:42 +0200

    refactor: add update profile avatar route to users router

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -7,12 +7,14 @@ import { getProfileHandler, getProfileRoute } from "./routes/get-profile";
 import { getUserHandler, getUserRoute } from "./routes/get-user";
 import { getUsersHandler, getUsersRoute } from "./routes/get-users";
 import { updateProfileHandler, updateProfileRoute } from "./routes/update-profile";
+import { updateProfileAvatarHandler, updateProfileAvatarRoute } from "./routes/update-profile-avatar";
 import { updateUserHandler, updateUserRoute } from "./routes/update-user";
 
 export const usersRouter = createRouter()
     .openapi(getProfileRoute, getProfileHandler)
     .openapi(updateProfileRoute, updateProfileHandler)
     .openapi(changePasswordRoute, changePasswordHandler)
+    .openapi(updateProfileAvatarRoute, updateProfileAvatarHandler)
     .openapi(createUserRoute, createUserHandler)
     .openapi(getUsersRoute, getUsersHandler)
     .openapi(getUserRoute, getUserHandler)
